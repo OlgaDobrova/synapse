b069b3ac607df33075c90f6dd8e22b85061b5844
OlgaDobrova <dobrova_o_g@mail.ru>
2021-05-19 00:07:40 +0300

    Слайдер+план дома

---
+++ assets/js/scripts.js
@@ -0,0 +1,49 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+   autoplay: {
+   delay: 5000,
+   },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+let menuToggle = $('.header-menu-toggle');
+menuToggle.on('click',function(event){
+  event.preventDefault();
+  $('.header-nav').slideToggle(200);
+})
+
+let contactsForm = $('.contacts-form');
+
+contactsForm.on('submit', function(event){
+  event.preventDefault(); //отменили стандартную отправку формы
+  alert();
+  var formData = new FormData(this); //здесь данные по всем 3м полям формы обр связи: имя, email и вопрос
+  formData.append('action','contacts_form'); //в форму обратной связи добавили атрибут action
+  //формируем запрос на сервер
+  $.ajax({
+    type: "POST",
+    url: adminAjax.url,
+    data: formData,
+    contentType: false,
+    processData: false,
+    success: function (response) {
+      console.log('Ответ сервера:' + response);
+    }
+  });
+});
--- front-page.php
+++ front-page.php
@@ -30,8 +30,84 @@
   </div><!--/.smeta-->
 </div><!--/.container-smeta-->
 <div class="container-chief"> <!--Завершится в footer-->
-  front-page.php
+  <h3 class="house-project-title"><?php the_title();?></h3>
+  <div class="house-project-wrapper"> <!--Слайдер + планы дома-->
+    <div class="house-project-swiper"><!--Слайдер-->
+      <?php
+        //Объявляем глобальную переменную post
+        global $post;
+        $query = new WP_Query([ 
+        	'posts_per_page' => 1,
+          'category_name'  => 'photoprogect',
+        ]);
+        //проверка на наличие постов
+        //если есть
+        if($query->have_posts()){
+          //то запускаем цикл
+        	while( $query->have_posts()){
+        		$query->the_post();
+      ?>
 
+      <!-- Слайдер Slider main container -->
+      <div class="swiper-container photo-house-project-slider">
+        <!-- Дополнительная необходимая обертка -->
+        <div class="swiper-wrapper">
+          <!-- Слайды -->
+          <!--get_attached_media выводит все картинки, которые прикреплены к посту-->
+          <?php $images = get_attached_media('image');
+            foreach ($images as $image) {
+              //guid - это ссылка на картинку
+              //print_r($image -> guid) - вывести ссылку на картинку
+              echo '<div class="swiper-slide"><img src="';
+              print_r($image -> guid);
+              echo '"></div>';
+            }
+          ?>
+        </div>
+        <div class="swiper-pagination"></div>
+         <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
 
+        <!-- If we need scrollbar -->
+        <div class="swiper-scrollbar"></div>
+      </div><!--/.swiper-container photo-report-slider-->
+          
+      <?php 
+        	}
+        } else {
+        	// Постов не найдено
+          ?>
+          <p>Постов не найдено</p>
+          <?php
+        }
+        wp_reset_postdata(); // Сбрасываем $post
+      ?>
+    </div><!--/.house-project-swiper/ -->
+    <div class="house-project-plan"><!--планы дома-->
+      <div class="house-project-plan-1">
+        <div class="house-project-plan-text">1 этаж</div>
+        <img src="http://synapse/wp-content/uploads/2021/05/etazh1n.png" alt="" class="house-project-plan-view">
+      </div>
+      <div class="house-project-plan-2">
+        <div class="house-project-plan-text">2 этаж</div>
+        <img src="http://synapse/wp-content/uploads/2021/05/etazhn2.png" alt="" class="house-project-plan-view">
+      </div>
+    </div><!--/.house-project-plan/ -->
+
+      <!--планы дома--> 
+
+        
+
+  </div><!--/.house-project-wrapper-->
+<img width=773px src="images/lake.jpg" alt="project overview picture">
+    <div class="slider">
+      <img src="images/arrow-left.png" alt="slider arrow - left">
+      <img src="<?php echo get_template_directory_uri().'/assets/images/lake1.png';?>" alt="smaller view picture">
+      <img src="<?php echo get_template_directory_uri().'/assets/images/lake2.png';?>" alt="smaller view picture">
+      <img src="<?php echo get_template_directory_uri().'/assets/images/lake3.png';?>" alt="smaller view picture">
+      <img src="<?php echo get_template_directory_uri().'/assets/images/lake4.png';?>" alt="smaller view picture">
+      <img src="images/arrow-right.png" alt="slider arrow - right">
+    </div>
 
 <?php get_footer();?>
